0364cdd8a102d3a2f5c37da828324db1fec5db8f
jinleileiking <jinleileiking@gmail.com>
2010-04-07 00:27:35 +0800

    utc

--- config/environment.rb
+++ config/environment.rb
@@ -36,7 +36,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-#  config.time_zone = 'UTC'
+  config.time_zone = 'UTC'
   config.action_controller.session_store = :active_record_store
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
